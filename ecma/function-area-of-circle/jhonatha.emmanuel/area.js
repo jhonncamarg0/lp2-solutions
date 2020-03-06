a8ad209e5ddc15a0b7b9e2262c75c86fc018d9ba
jhonncamarg0 <jhonatha.emmanuel@academico.ifpb.edu.br>
2020-03-06 11:47:36 -0300

    Atividades

---
+++ ecma/function-area-of-circle/jhonatha.emmanuel/area.js
@@ -0,0 +1,9 @@
+let r = 10;
+
+const pi = 3.14;
+
+function area(a, b) {
+    return a * (b**2);
+}
+
+console.log(area(pi, r));
---
+++ ecma/function-calendar/jhonatha.emmanuel/calendar.js
@@ -0,0 +1,12 @@
+console.log("DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB");
+
+for (let i = 0 ; i < 3 ; i++); {
+    
+    e = " "
+
+    for(let j = 0 ; j < 10 ; j++); {
+        e += i.toString()+j.toString()+"  "
+    }
+
+    console.log(e);
+}

6dda2581ad6e97957a39c83a86cc96aa2f156e97
jhonncamarg0 <jhonatha.emmanuel@academico.ifpb.edu.br>
2020-03-06 11:54:11 -0300

    Atividades

--- ecma/function-calendar/jhonatha.emmanuel/calendar.js
+++ ecma/function-calendar/jhonatha.emmanuel/calendar.js
@@ -1,12 +1,47 @@
 console.log("DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB");
 
-for (let i = 0 ; i < 3 ; i++); {
+for (let i = 0 ; i < 1 ; i++); {
     
     e = " "
 
-    for(let j = 0 ; j < 10 ; j++); {
+    for(let j = 1 ; j < 8 ; j++); {
         e += i.toString()+j.toString()+"  "
     }
 
     console.log(e);
 }
+
+for (let i = 0 ; i < 1 ; i++); {
+    
+    e = " "
+
+    for(let j = 1 ; j < 8 ; j++); {
+        e += i.toString()+j.toString()+"  "
+    }
+
+    console.log(e);
+}
+
+for (let i = 0 ; i < 1 ; i++); {
+    
+    e = " "
+
+    for(let j = 1 ; j < 8 ; j++); {
+        e += i.toString()+j.toString()+"  "
+    }
+
+    console.log(e);
+}
+
+for (let i = 0 ; i < 1 ; i++); {
+    
+    e = " "
+
+    for(let j = 1 ; j < 8 ; j++); {
+        e += i.toString()+j.toString()+"  "
+    }
+
+    console.log(e);
+}
+
+/* está em andamento */

407f112acd943363170d2f5d10c26d9fc9330fc3
jhonncamarg0 <jhonatha.emmanuel@academico.ifpb.edu.br>
2020-10-12 21:28:08 -0300

    update

--- atividades-remotas/template/country-flags-sqlite/src/migrations/index.js
+++ atividades-remotas/template/country-flags-sqlite/src/migrations/index.js
@@ -1,7 +1,15 @@
 const { conn } = require('../db');
 
 async function up() {
-  // TODO
+  const db = await conn();
+
+  await db.run(`
+    CREATE TABLE IF NOT EXISTS flags (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      image TEXT,
+      name TEXT
+    )
+  `);
 }
 
 async function down() {
--- atividades-remotas/template/country-flags-sqlite/src/models/Flag.js
+++ atividades-remotas/template/country-flags-sqlite/src/models/Flag.js
@@ -1,15 +1,49 @@
 const { conn } = require('../db');
 
 async function create(data) {
-  // TODO
+  const sql = `
+    INSERT INTO
+      flags (id, name, image)
+    VALUES
+      (?, ?, ?)
+  `;
+
+  const db = await conn();
+
+  const { id, name, image } = data;
+
+  const { lastID } = await db.run(sql, [ id, name, image ]);
+
+  return lastID;
 }
 
 async function readAll() {
-  // TODO
+
+  const sql =`
+    SELECT
+      flags.id, flags.image, flags.name
+  `;
+
+  const db = await conn();
+
+  const flags = await db.all(sql);
+
+  return flags;
 }
 
 async function readById(id) {
-  // TODO
+
+  const sql =`
+    SELECT
+      flags.id, flags.image, flags.name
+  `;
+
+  const db = await conn();
+
+  const foods = await db.all(sql [ id ]);
+
+  return foods;
+
 }
 
 module.exports = { create, readAll, readById };
